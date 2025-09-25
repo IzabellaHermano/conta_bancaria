f30c1742dbaf9f93594a7255a887ba3eb2f8b742
Izabella Hermano <izabellahermano1@gmail.com>
2025-09-25 11:08:37 -0300

    .

--- src/main/java/com/senai/conta_bancaria/application/service/ContaService.java
+++ src/main/java/com/senai/conta_bancaria/application/service/ContaService.java
@@ -1,28 +1,33 @@
 package com.senai.conta_bancaria.application.service;
 
 import com.senai.conta_bancaria.application.dto.ContaResumoDTO;
+import com.senai.conta_bancaria.domain.entity.Conta;
 import com.senai.conta_bancaria.domain.repository.ContaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ContaService {
 
     private final ContaRepository repository;
 
+    @Transactional(readOnly = true)
     public List<ContaResumoDTO> listarContasAtivas(){
      return repository.findAllByAtivaTrue().stream()
              .map(ContaResumoDTO::fromEntity)
              .toList();
     }
 
+    @Transactional(readOnly = true)
     public ContaResumoDTO buscarContaAtivaPorNumero(String numero) {
-        var conta = repository.findByNumeroAndAtivaTrue(numero).orElseThrow(
-                () -> new RuntimeException("Conta não encontrada")
-        );
-        return ContaResumoDTO.fromEntity(conta);
+        return ContaResumoDTO.fromEntity(
+                repository.findByNumeroAndAtivaTrue(numero)
+                        .orElseThrow(() -> new RuntimeException("Conta não encontrada")
+        ));
     }
 }
--- src/main/java/com/senai/conta_bancaria/domain/repository/ClienteRepository.java
+++ src/main/java/com/senai/conta_bancaria/domain/repository/ClienteRepository.java
@@ -10,6 +10,5 @@ import java.util.Optional;
 @Repository
 public interface ClienteRepository extends JpaRepository <Cliente, String> {
     Optional<Cliente> findByCpfAndAtivoTrue(String cpf);
-
     List <Cliente> findAllByAtivoTrue();
 }
